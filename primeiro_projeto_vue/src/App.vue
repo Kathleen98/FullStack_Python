f1150f00cd51bc389b26fa0e1ef7e5c2f50e5917
Kathleen98 <99849068+Kathleen98@users.noreply.github.com>
2024-01-05 12:53:39 -0300

    Módulo Vue

---
+++ primeiro_projeto_vue/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  const nome = "Kethleen";
+  const meuObjeto = {
+    nome: "Kathleen",
+    desenhoFavorito: "One Piece"
+  }
+
+  function oi(nome){
+    return `${nome} diz oi`
+  }
+
+  const zorinho = "https://i.pinimg.com/564x/3e/fe/40/3efe40fd87a857985f3aa9d3f13b9c1f.jpg"
+</script>
+
+<template>
+  <h1>{{oi()}}</h1>
+  <img :src="zorinho"/>
+</template>
+
+<style scoped>
+</style>
